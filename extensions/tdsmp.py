d46c8638a598c7ee8f017ed0bc3df58c4465dc86
cups9 <cups.threatencups.threaten031@passinbox.com>
2024-12-13 18:44:04 +1100

    Rebrand to /bin/crescent + many changes with all commands grouped e.g. debug, smp (13.12.24) // (DD/MM/YY)

--- extensions/tdsmp.py
+++ extensions/tdsmp.py
@@ -1,24 +1,37 @@
+# -- IMPORTS -- #
+# Dependencies required
 import arc
+from typing import Any
 
+# -- QUICKSTART --
+# Defines the `plugin` variable for other commands
 plugin = arc.GatewayPlugin("tdsmp")
+tdsmp = plugin.include_slash_group("smp", "Commands from the active SMP from Citrine Studios")
 
-@plugin.include
-@arc.slash_command("modpack-ver", "List the current modpack version.")
+# Uses @plugin.include instead of @client.include
+@tdsmp.include
+@arc.slash_subcommand("modpack-ver", "List the current SMP modpack version.")
 async def mrpack(
     ctx: arc.GatewayContext
 ) -> None:
     await ctx.respond(f"**TDSMP**\nThe current modpack version is: **v1.1.0**. (last updated: <t:1734003240:D>)")
 
-# This is currently **very** messy. Thinking of implementing this in an embed instead.
-# --DELETE WHEN FINISHED-- Queued for testing outside of codespace and on local machine.
-@plugin.include
-@arc.slash_command("rules", "List the current rules for TDSMP.")
+# This is currently **slightly** messy (implemented it as a docstring instead). Thinking of implementing this in an embed instead.
+@tdsmp.include
+@arc.slash_subcommand("rules", "List the current rules for the SMP.")
 async def rules(
     ctx: arc.GatewayContext
 ) -> None:
-    await ctx.respond(
-        f"# The TDSMP Rules and Amendments\nThou shalt not kill or injure those for devious reasons or intentions regarding harm to reputation, the victim, or overall server status\nThou shalt not thieve in one's personal belongings, and must require consent from the other individual or people\nThou shalt not conduct destruction of property regarding self or others in harmful or leisure intentions\nShould any rule be broken in the slightest manner, punishment of the individual will be determined by the severity of the incident, along with a server vote.")
+    await ctx.respond("""
+        # The TDSMP Rules and Amendments
+        1. Thou shalt not kill or injure those for devious reasons or intentions regarding harm to reputation, the victim, or overall server status
+        2. Thou shalt not thieve in one's personal belongings, and must require consent from the other individual or people
+        3. Thou shalt not conduct destruction of property regarding self or others in harmful or leisure intentions
+        4. Should any rule be broken in the slightest manner, punishment of the individual will be determined by the severity of the incident, along with a server vote.
+(Originally written by DavidPlanks)
+        """)
 
+# Adds the plugin to the client (main.py)
 @arc.loader
 def loader(client: arc.GatewayClient) -> None:
     client.add_plugin(plugin)
--- main.py
+++ main.py
@@ -1,29 +1,69 @@
-# List of Imports
+"""
+/bin/crescent - @cups9's open-source utility Discord bot
+* this discord bot was intended to be used for Citrine Studios projects, such as TDSMP
+* all code here is modifiable so long as you adhere to the GPL-3 license (you must fork this repo to modify the code)
+* if this project expands, it will be owned by Citrine Studios and not @cups9
+"""
+# -- IMPORTS -- #
+# `hikari` and `arc` are explained in the README.md
 import hikari
 import arc
-# "import hidden" is ignored in .gitignore for security purposes
+# "import hidden" is disabled in .gitignore for security purposes
 import hidden
+# required for certain functions
+from typing import Any
 
-# Initialisation
+# -- QUICKSTART -- #
+# `bot` and `client` define key variables used within the script
 bot = hikari.GatewayBot(token=hidden.TOKEN)
 client = arc.GatewayClient(bot)
 
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print(event.content)
-
+# Outputs a custom message for when the bot starts, allowing for easy readability
 @bot.listen(hikari.StartedEvent)
 async def bot_started(event):
-    print('/bin/utils is now active.')
+    print('[⭐️] [LOG] /bin/utils is now active.')
 
 # -- COMMANDS -- #
-
+# Test command pulled from the `arc` example repo (thank you!)
 @client.include
 @arc.slash_command("hi", "Say hi!")
-async def ping(
+async def test(
     ctx: arc.GatewayContext,
     user: arc.Option[hikari.User, arc.UserParams("The user to say hi to.")]
 ) -> None:
     await ctx.respond(f"Hey {user.mention}!")
 
+# -- DEBUG -- #
+# Defines the slash command group for the next few commands
+status = client.include_slash_group("status", "Check the status of the bot")
+
+# Checks the ping of the bot
+@status.include
+@arc.slash_subcommand("ping", "Check the bot's ping.")
+async def ping(
+    ctx: arc.GatewayContext
+) -> None:
+    await ctx.respond(f"Pong! {bot.heartbeat_latency * 1_000:.0f}ms")
+
+# Checks the shard (instances/servers) count of the bot
+@status.include
+@arc.slash_subcommand("shard-count", "Check the bot's shard count.")
+# Checks if the command has the right author
+# [#] @cups9 : currently attempting to somehow declare the `author_check` function so it can be used in any command, need help!
+async def author_check(ctx: arc.Context[Any]) -> None:
+    if ctx.author.id != hidden.AUTHOR_ID: # the author is hidden for privacy purposes
+        raise Exception("An unauthorised author attempted to execute this command.")
+async def shard_count(
+    ctx: arc.GatewayContext
+) -> None:
+    await ctx.respond(f"This bot currently has: **{bot.shard_count}** shards.", flags=hikari.MessageFlag.EPHEMERAL)
+
+# -- EXTENSIONS -- #
+# Loads the extensions required for this project, which can be unloaded later if change is desired
+# Imports the module from a different script which in this case, are scripts from the "extensions" folder
+client.load_extension("extensions.tdsmp")
+
+# -- LAUNCH -- #
+# Launches and starts the bot
+# !! PLEASE KEEP THIS AT THE BOTTOM !!
 bot.run()
